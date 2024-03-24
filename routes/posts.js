996179fb26fa9fcc9a7af14d6d1727026c1c4578
manikant153 <manikantgiri1@gmail.com>
2024-03-25 01:02:20 +0530

    display the post when user is sign-in

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
+const passport = require('passport');
 
 const Post_controller = require('../controllers/post_controller')
-router.post('/create',Post_controller.create);
+router.post('/create',passport.checkAuthentication,Post_controller.create);
 
 module.exports = router;
